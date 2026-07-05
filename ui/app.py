69f883ed78f2ab757661ccc43cf9620ba7bdd814
MUHAMMADNOMANZAHID804 <nomanzahid5911@gmail.com>
2026-07-05 16:15:29 +0500

    Update app.py

--- ui/app.py
+++ ui/app.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import streamlit as st
 from core.manager import PipelineManager
-from tools.scrapper_mcp import Scrape
+from tools.scrapper_mcp import ScraperMCP
 """
 ui/app.py
 ---------
